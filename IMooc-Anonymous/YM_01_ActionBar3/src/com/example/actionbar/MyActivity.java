f4dd6afdad4c8268e41005edc3fefa829384a51b
LaoTongSH <laotong@sina.com>
2017-12-18 15:20:49 +0800

    Add files via upload

---
+++ IMooc-Anonymous/YM_01_ActionBar3/src/com/example/actionbar/MyActivity.java
@@ -0,0 +1,64 @@
+package com.example.actionbar;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MyActivity extends Activity {
+    private MenuItem menuItem;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM
+            | ActionBar.DISPLAY_SHOW_HOME
+            | ActionBar.DISPLAY_SHOW_TITLE);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_load:
+                menuItem = item;
+                menuItem.setActionView(R.layout.progressbar);
+                menuItem.expandActionView();
+                TestTask testTask = new TestTask();
+                testTask.execute("text");
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+
+    private class TestTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            // Simulate something long running
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            menuItem.collapseActionView();
+            menuItem.setActionView(null);
+        }
+    };
+}
