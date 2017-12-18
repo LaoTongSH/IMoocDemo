e69e84f74d1e7ece9e4f62bbfd0862f73151239e
LaoTongSH <laotong@sina.com>
2017-12-18 15:24:03 +0800

    Add files via upload

---
+++ IMooc-Anonymous/YM_01_ActionBar4/src/com/example/actionbar/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.actionbar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2014/6/6.
+ */
+public class MainActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello);
+    }
+}
---
+++ IMooc-Anonymous/YM_01_ActionBar4/src/com/example/actionbar/SecondActivity.java
@@ -0,0 +1,67 @@
+package com.example.actionbar;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class SecondActivity extends Activity {
+    private MenuItem menuItem;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        ActionBar actionBar = getActionBar();
+        actionBar.setHomeButtonEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_load:
+                menuItem = item;
+                menuItem.setActionView(R.layout.progressbar);
+                menuItem.expandActionView();
+                TestTask testTask = new TestTask();
+                testTask.execute("text");
+                break;
+            case android.R.id.home:
+                Intent intent = new Intent(this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            default:
+                break;
+        }
+        return true;
+    }
+
+    private class TestTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            // Simulate something long running
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            menuItem.collapseActionView();
+            menuItem.setActionView(null);
+        }
+    };
+}
